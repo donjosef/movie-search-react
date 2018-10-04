f1a92e7a4c5f997d9b58751da1fb3e293cecec8f
donjoe <giumontanaro@gmail.com>
2018-10-04 19:37:55 +0200

    MovieInfo component will display all the info relative to movie

---
+++ src/components/MovieInfo/MovieInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const movieInfo = props => {
+    return (
+      <article>
+        <img />
+         <h1>title</h1>
+         <h3>Intro title</h3>
+         <p>Plot</p>
+         <h4></h4>
+         <div>Info</div>
+         <div>Info</div>
+         <div>Info</div>
+         <div>Info</div>
+      </article>
+    )
+}
+
+export default movieInfo;
