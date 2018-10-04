bbd5e1ee38b91601808fe45308114a20713b774a
donjoe <giumontanaro@gmail.com>
2018-10-04 19:36:18 +0200

    Render searchMovie and movieInfo inside App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import SearchMovie from './components/SearchMovie/SearchMovie';
+import MovieInfo from './components/MovieInfo/MovieInfo'
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    inputVal: ''
+  }
+
+  changeInputHandler = (e) => {
+    this.setState({ inputVal: e.target.value });
+  }
+
+
   render() {
     return (
       <div className="App">
-      
+        <header>
+          Logo
+          <SearchMovie inputVal={this.state.inputVal} changeInput={this.changeInputHandler}/>
+        </header>
+        <MovieInfo />
       </div>
     );
   }
