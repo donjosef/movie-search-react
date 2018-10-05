21e5343181723a21909eb79af995161eb3184ee0
donjoe <giumontanaro@gmail.com>
2018-10-06 01:30:36 +0200

    Add a CSSTransition component to animate the list of movies

--- src/components/ListResults/ListResults.js
+++ src/components/ListResults/ListResults.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import CSSTransition from 'react-transition-group/CSSTransition';
+
 import './ListResults.css';
 
 const listResults = props => {
@@ -14,13 +16,19 @@ const listResults = props => {
     }
     return (
       <div>
-       {displayedMovies.length > 0 && (
-         <ul className="ListResults" style={dynamicHeight}>
-            {displayedMovies}
-         </ul>
-       )}
+         <CSSTransition
+              in={displayedMovies.length > 0}
+              timeout={500}
+              mountOnEnter
+              unmountOnExit
+              classNames='translateY'>
+           <ul className="ListResults" style={dynamicHeight}>
+              {displayedMovies}
+           </ul>
+         </CSSTransition>
       </div>
     )
+
 }
 
 export default listResults;
