14773bc3190ee527352f8180dab730a2275c327a
donjoe <giumontanaro@gmail.com>
2018-10-06 00:38:51 +0200

    Set a dynamic height on listResults based on results

--- src/components/ListResults/ListResults.js
+++ src/components/ListResults/ListResults.js
@@ -5,11 +5,17 @@ const listResults = props => {
     const {movies, getMovie} = props;
     const displayedMovies = movies.map(movie => (
         <li key={movie.id} onClick={() => getMovie(movie.id)}>{movie.title}</li>
-    ))
+    ));
+    let dynamicHeight = null;
+    if(displayedMovies.length <= 6) {
+      dynamicHeight = {
+        height: 'auto'
+      };
+    }
     return (
       <div>
        {displayedMovies.length > 0 && (
-         <ul className="ListResults">
+         <ul className="ListResults" style={dynamicHeight}>
             {displayedMovies}
          </ul>
        )}
