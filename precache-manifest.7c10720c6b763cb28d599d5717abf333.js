3924a2ca43bbf42112b375673737a68f5b00efbb
donjoe <giumontanaro@gmail.com>
2018-10-12 19:24:27 +0200

    Updates

---
+++ precache-manifest.7c10720c6b763cb28d599d5717abf333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1edc35f0fa63d0e0b3c9bc5537de942",
+    "url": "/movie-search-react/static/media/no-image-png-1.b1edc35f.png"
+  },
+  {
+    "revision": "1242073ae4980314a0c0",
+    "url": "/movie-search-react/static/js/runtime~main.1242073a.js"
+  },
+  {
+    "revision": "701db058063a919b1e66",
+    "url": "/movie-search-react/static/js/main.701db058.chunk.js"
+  },
+  {
+    "revision": "af171ab5b0176e795718",
+    "url": "/movie-search-react/static/js/1.af171ab5.chunk.js"
+  },
+  {
+    "revision": "701db058063a919b1e66",
+    "url": "/movie-search-react/static/css/main.e7696894.chunk.css"
+  },
+  {
+    "revision": "b466249528cc63cddbe344aa56c9f24e",
+    "url": "/movie-search-react/index.html"
+  }
+];
